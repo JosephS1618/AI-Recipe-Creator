8d04227eec602e8a08907b62a77eaf181d648a65
runjuu <hi@runjuu.com>
2026-02-20 22:11:38 -0800

    chore(frontend): setup shadcn & tailwindcss

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,3 +1,5 @@
+import path from "node:path";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
@@ -9,7 +11,13 @@ export default defineConfig({
 				plugins: [["babel-plugin-react-compiler"]],
 			},
 		}),
+		tailwindcss(),
 	],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
 	server: {
 		proxy: {
 			"/api": "http://localhost:3000",
