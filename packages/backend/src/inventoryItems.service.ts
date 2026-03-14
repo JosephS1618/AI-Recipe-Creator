68f2275fc905b60c562525282ca370027eaf99cc
alijaved <alijavedansari@gmail.com>
2026-03-14 15:52:11 -0700

    feat(inventoryItem): update add backend logic

--- packages/backend/src/inventoryItems.service.ts
+++ packages/backend/src/inventoryItems.service.ts
@@ -64,7 +64,7 @@ export class InventoryItemsService {
 				ReceiptID AS receipt_id
 			FROM InventoryItem
 			WHERE InventoryID = ${inventory_id}
-			ORDER BY InventoryItemID ASC;
+			ORDER BY InventoryItemID;
 		`;
 	}
 
